96366f939b6e3280d8521cbc5a6f86e30d14743d
James Devlin <devlin.ja@northeastern.edu>
2021-01-06 03:33:17 -0500

    Created prototype parser
    Parses lines correctly, but does not iterate yet. Is not the most
    efficient possible, but currently works as expected.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+use std::cmp;
+use std::io::BufRead;
+
+#[derive(Debug)]
 pub struct Count {
     lines: usize,
     words: usize,
@@ -14,6 +18,21 @@ impl Count {
             chars: 0,
             bytes: 0,
             line_len: 0,
+        }
+    }
+}
+
+impl Count {
+    pub fn process<R: BufRead>(&mut self, mut reader: R) {
+        let mut line = String::new();
+
+        let amt_read = reader.read_line(&mut line).expect("Could not read line...");
+        if amt_read > 0 {
+            self.lines += 1;
+            self.bytes += amt_read;
+            self.line_len = cmp::max(self.line_len, line.len());
+            self.words += line.split_whitespace().count();
+            self.chars += line.chars().count();
         }
     }
 }
