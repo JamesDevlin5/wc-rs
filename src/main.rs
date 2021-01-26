ecc901529167da6696e46d123318d1bfaceef07f
James Devlin <devlin.ja@northeastern.edu>
2021-01-25 20:36:55 -0500

    Created prototype
    Defined a working prototype that extracts config information from the
    arguments, and parses a file named `t` in the cwd.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use args::Config;
+use std::fs::File;
+use wc_rs::Count;
+
+mod args;
+
 fn main() {
-    println!("Hello, world!");
+    let cfg = Config::new();
+    println!("{:?}", cfg);
+    let mut c = Count::new();
+    let f = File::open("t").unwrap();
+    c.process(f);
+    println!("{:?}", c);
 }
